f9cf8b34125072f5f1fa281c74f54a8b891fa5e7
Thiago Silva Oliveira <thiago.einteinnnn@gmail.com>
2018-11-06 00:52:07 -0300

    create, delete em app

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
-const router = express.Router()
+const router = express.Router();
+
+app.use(bodyParser.json()) // Todo conteudo sera convertido para Json
+app.use(bodyParser.urlencoded({ extended: false})) // Codificar as URL
 
 const route = router.get('/', (req, res,next) => {
     res.status(200).send({
-        title: "Node Store API",
+        title: "Node Store API Kryptech",
         version: "0.0.1"
     })
 })
 
+const create = router.post('/', (req, res,next) => { //req = requisição, res = resposta
+    res.status(201).send(req.body)
+})
+
+const put = router.put('/:id', (req, res,next) => {
+    const id = req.params.id // Recuperar parametros que virão pela URL
+    res.status(200).send({
+        id: id,
+        item: req.body
+    })
+})
+
+const del = router.delete('/', (req, res,next) => { //req = requisição, res = resposta
+    res.status(200).send(req.body)
+})
+
 app.use('/', route)
+app.use('/products', create)
+app.use('/products', put)
 
 module.exports = app
